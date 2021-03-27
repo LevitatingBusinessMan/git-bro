ecb135a5ad9a7434f2b56db8fae229dc1912a87f
LevitatingBusinessMan <public@reinfernhout.xyz>
2021-03-27 02:59:51 +0100

    initial

---
+++ git-bro.rb
@@ -0,0 +1,71 @@
+#!/usr/bin/ruby
+require "tomlrb"
+require "fileutils"
+require "notify"
+
+=begin
+TODO
+
+branches
+supporting repos with the same name but different author
+support deleting repos not mentioned in config
+
+maybe better toml syntax:
+[name]
+url=
+branch=
+
+=end
+
+def notify(repo, msg)
+	Notify.notify "git-bro: #{repo}", msg, { app_name: "git-bro" }
+end
+
+config = Tomlrb.load_file "#{Dir.home}/.config/git-bro/config.toml"
+
+#Make sure we have a repos folder
+REPOS_DIR = "#{Dir.home}/.local/share/git-bro/repos"
+FileUtils.mkdir_p REPOS_DIR
+
+repos = []
+for repo in Dir.entries(REPOS_DIR)
+	next if repo == "." or repo == ".."
+	puts "Found #{repo}"
+	repos.push repo
+	#url = `git --git-dir #{REPOS_DIR}/#{repo}/.git config --get remote.origin.url`.delete_suffix "\n"
+	fetch = `2>&1 git --git-dir #{REPOS_DIR}/#{repo}/.git fetch origin`
+	
+	if $?.exitstatus != 0
+		STDERR.puts "Failed to fetch #{repo}:\n#{fetch}"
+		notify(repo, "Failed to fetch")
+		next
+	end
+
+	#New stuff fetched
+	if fetch != ""
+		puts "New commits on #{repo}"
+		notify(repo, "New commits found")
+	end
+
+end
+
+#make sure all repos exist
+for url in config["repos"]
+	name = url.split("/").last.delete_suffix ".git"
+	
+	#clone repo
+	if !repos.include?(name)
+		puts "Initializing #{name}"
+		notify(name, "Repo #{name} is missing. Cloning it now.")
+		clone = `git clone #{url} #{REPOS_DIR}/#{name}`
+		if $?.exitstatus != 0
+			STDERR.puts "Failed to clone #{name}:\n#{clone}"
+			notify(name, "Failed to clone")
+			next
+		end
+	end
+end
+
+def run_scripts()
+	p "scripts are WIP"
+end
