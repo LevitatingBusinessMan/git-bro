d63ff6cbaa4b200c4a54739e3ab39a190e7b2072
LevitatingBusinessMan (Rein Fernhout) <me@levitati.ng>
2022-10-04 16:35:06 +0200

    Improve install script

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-install -Dvm 755 git-bro.rb $HOME/.local/bin/git-bro
-install -DCv systemd/git-bro.service $HOME/.local/share/systemd/user/git-bro.service
-install -DCv systemd/git-bro.timer $HOME/.local/share/systemd/user/git-bro.timer
+: ${PREFIX:=$HOME/.local}
+
+install -Dvm 755 git-bro.rb $PREFIX/bin/git-bro
+install -DCv systemd/git-bro.service $PREFIX/share/systemd/user/git-bro.service
+install -DCv systemd/git-bro.timer $PREFIX/share/systemd/user/git-bro.timer
 
 #Setup config
 mkdir -pv $HOME/.config/git-bro/scripts
