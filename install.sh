8d9d50ba6aa3a46445eae760d06d2943e0492be3
LevitatingBusinessMan (Rein Fernhout) <me@levitati.ng>
2022-10-05 02:01:51 +0200

    system systemd files

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 : ${PREFIX:=$HOME/.local}
 
 install -Dvm 755 git-bro.rb $PREFIX/bin/git-bro
-install -Dvm 644 systemd/git-bro.service $PREFIX/share/systemd/user/git-bro.service
-install -Dvm 644 systemd/git-bro.timer $PREFIX/share/systemd/user/git-bro.timer
+install -Dvm 644 systemd/user/git-bro.service $PREFIX/share/systemd/user/git-bro.service
+install -Dvm 644 systemd/user/git-bro.timer $PREFIX/share/systemd/user/git-bro.timer
 
 #Setup config
 mkdir -pv $HOME/.config/git-bro/scripts
